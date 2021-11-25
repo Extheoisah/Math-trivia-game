3e31f4957e4e521cb6510a2103f14b146b305d5a
Baribor Saturday <saturdaybaribor@gmail.com>
2021-11-25 21:07:19 +0100

    test changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import HomeComponent from "./components/HomeComponent/HomeComponent";
-import GameStartComponent from "./components/GameComponent/GameStartComponent";
-import GameEndComponent from "./components/GameComponent/GameEndComponent";
 import "./App.css";
 
 import TriviaContext from "./context";
@@ -14,11 +11,7 @@ function App() {
   return (
 
     <div className="App">
-      {/* <TriviaContext.Provider value={{ gameState, setGameState, score, setScore}}>
-        {gameState === "home" && <HomeComponent />}
-        {gameState === "start" && <GameStartComponent />}
-        {gameState === "end" && <GameEndComponent />}
-      </TriviaContext.Provider> */}
+      
       <TriviaContext.Provider value={{ gameState, setGameState, score, setScore }}>
         <Outlet />
       </TriviaContext.Provider>
